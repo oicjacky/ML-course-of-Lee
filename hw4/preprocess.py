fc62a2fa82a05e79dd9862869c2cc4244d80d2a8
oicjacky <610611105@gms.ndhu.edu.tw>
2022-02-01 22:51:12 +0800

    Add BagOfWord implemented by gensim.corpora.Dictionary

--- hw4/preprocess.py
+++ hw4/preprocess.py
@@ -1,14 +1,17 @@
-import os, pdb
+import os
 import torch
+import gensim
 from gensim.models import Word2Vec
 
 
 class Preprocess():
     '''
-    0. Initialize with `make_embedding`
-    1. Then, generate X and Y with
-        X = Preprocess().sentence_word2idx()
-        Y = Preprocess().labels_to_tensor(y)
+    Steps:
+        0. Initialize `Word2vec` model and
+            word embedding look-up matrix with `make_embedding`.
+        1. Then, generate X and Y by
+            X = `sentence_word2idx` with shape (num_data, feature_dim)
+            Y = `labels_to_tensor`  with shape (num_data,)
     '''
     def __init__(self, sentences, sen_len, w2v_path="./w2v.model"):
         self.w2v_path = w2v_path
@@ -19,13 +22,14 @@ class Preprocess():
         self.embedding_matrix = []
     
     def get_w2v_model(self):
-        # 把之前訓練好的 word to vec 模型讀進來
+        ''' Load pre-trained `Word2Vec` model. '''
         self.embedding = Word2Vec.load(self.w2v_path)
         self.embedding_dim = self.embedding.vector_size
     
     def add_embedding(self, word):
-        # 把 word 加進 embedding，並賦予他一個隨機生成的 representation vector
-        # word 只會是 "<PAD>" 或 "<UNK>"
+        ''' Set a randomized representation vector to these word embeddings.
+        It must be "<PAD>" or "<UNK>".
+        '''
         vector = torch.empty(1, self.embedding_dim)
         torch.nn.init.uniform_(vector)
         self.word2idx[word] = len(self.word2idx)
@@ -63,7 +67,6 @@ class Preprocess():
         # 將每個句子變成一樣的長度
         if len(sentence) > self.sen_len:
             sentence = sentence[:self.sen_len]
-            # import pdb; pdb.set_trace()
         else:
             pad_len = self.sen_len - len(sentence)
             for _ in range(pad_len):
@@ -93,17 +96,39 @@ class Preprocess():
         return torch.LongTensor(y)
 
 
-# data.py
-# 實作了 dataset 所需要的 '__init__', '__getitem__', '__len__'
-# 好讓 dataloader 能使用
+class BagOfWord:
+
+    def __init__(self, sentences):
+        self.sentences = sentences
+        self.vocab = gensim.corpora.Dictionary(sentences) # `sentences` is tokenized documents
+        self._check_memory()
+
+    def _check_memory(self, threshold=1e8):
+        vector_len = len(self.sentences)*len(self.vocab)
+        assert vector_len < threshold, f"To large dimension of input data {vector_len}!"
+
+    def sentence_word2idx(self):
+        ''' Return torch.Tensor with shape (len(sentences), len(vocab)) '''
+        sentence_list = []
+        for sen in self.sentences:
+            bow_vector = torch.zeros((len(self.vocab), 1), dtype=torch.int8)
+            for id, val in self.vocab.doc2bow(sen):
+                bow_vector[id] = val
+            sentence_list.append(bow_vector)
+        return torch.cat(sentence_list, dim=1).T
+    
+    def labels_to_tensor(self, y):
+        ''' The same as `Preprocess.labels_to_tensor` '''
+        y = [int(label) for label in y]
+        return torch.LongTensor(y)
+
 
 class TwitterDataset(torch.utils.data.Dataset):
-    """
-    Expected data shape like:(data_num, data_len)
+    """ Expected data shape like:(data_num, feature_dim).
     Data can be a list of numpy array or a list of lists
-    input data shape : (data_num, seq_len, feature_dim)
-    
-    __len__ will return the number of data
+    Methods:
+        __len__ will return the number of data.
+        __getitem__ return the data by index.
     """
     def __init__(self, X, y):
         self.data = X
@@ -115,8 +140,6 @@ class TwitterDataset(torch.utils.data.Dataset):
         return len(self.data)
 
 
-
-
 if __name__ == '__main__':
 
     from utils import load_training_data, TRAIN_DATA_PATH
@@ -125,9 +148,15 @@ if __name__ == '__main__':
     sen_len = 20
     batch_size = 128
 
-    # 對 input 跟 labels 做預處理
-    preprocess = Preprocess(train_x, sen_len, w2v_path=w2v_path)
-    embedding = preprocess.make_embedding(load=True)
+    #NOTE: Preprocess
+    # preprocess = Preprocess(train_x, sen_len, w2v_path=w2v_path)
+    # embedding = preprocess.make_embedding(load=True)
+    # train_x = preprocess.sentence_word2idx()
+    # y = preprocess.labels_to_tensor(y)
+    
+    #NOTE: Bag of Word
+    train_x, y = train_x[:5000], y[:5000]
+    preprocess = BagOfWord(train_x)
     train_x = preprocess.sentence_word2idx()
     y = preprocess.labels_to_tensor(y)
 
@@ -138,9 +167,10 @@ if __name__ == '__main__':
                                                 shuffle = True,
                                                 num_workers = 2)
     for i, (inputs, labels) in enumerate(train_loader):
-        print(i)
-        print(inputs, labels)
+        print(f'[{i}/{len(train_loader)}] batch data with size {batch_size}')
+        print(inputs, inputs.size(), labels, sep='\n')
         if i == 2:
+            import pdb
             pdb.set_trace()
 
     print("Done")
