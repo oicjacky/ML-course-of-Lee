75b2ec645fc80caa306859f5e39f6dbf4517c962
oicjacky <610611105@gms.ndhu.edu.tw>
2022-02-07 15:35:08 +0800

    Add VGG16_half that use half layers of VGG16

--- hw3/model.py
+++ hw3/model.py
@@ -125,3 +125,84 @@ class VGG16(nn.Module):
         out = self.cnn(x)
         out = out.view(out.size()[0], -1)
         return self.fc(out)
+
+
+class VGG16_half(nn.Module):
+    def __init__(self,
+                 linear_batch_norm:bool = False,
+                 linear_dropout:bool = False, linear_dropout_rate:float = 0.5):
+        super(VGG16_half, self).__init__() 
+        self.linear_batch_norm = linear_batch_norm
+        self.linear_dropout = linear_dropout
+        self.linear_dropout_rate = linear_dropout_rate
+        # input [3, 128, 128]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),  # [64, 128, 128]
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, 1, 1),  # [64, 128, 128]
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [64, 64, 64]
+            
+            nn.Conv2d(64, 128, 3, 1, 1), # [128, 64, 64]
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, 1, 1), # [128, 64, 64]
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [128, 32, 32]
+
+            nn.Conv2d(128, 256, 3, 1, 1), # [256, 32, 32]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1), # [256, 32, 32]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            # nn.Conv2d(256, 256, 3, 1, 1), # [256, 32, 32]
+            # nn.BatchNorm2d(256),
+            # nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [256, 16, 16]
+
+            # nn.Conv2d(256, 512, 3, 1, 1), # [512, 16, 16]
+            # nn.BatchNorm2d(512),
+            # nn.ReLU(),
+            # nn.Conv2d(512, 512, 3, 1, 1), # [512, 16, 16]
+            # nn.BatchNorm2d(512),
+            # nn.ReLU(),
+            # nn.Conv2d(512, 512, 3, 1, 1), # [512, 16, 16]
+            # nn.BatchNorm2d(512),
+            # nn.ReLU(),
+            # nn.MaxPool2d(2, 2, 0),       # [512, 8, 8]
+            
+            # nn.Conv2d(512, 512, 3, 1, 1), # [512, 8, 8]
+            # nn.BatchNorm2d(512),
+            # nn.ReLU(),
+            # nn.Conv2d(512, 512, 3, 1, 1), # [512, 8, 8]
+            # nn.BatchNorm2d(512),
+            # nn.ReLU(),
+            # nn.Conv2d(512, 512, 3, 1, 1), # [512, 8, 8]
+            # nn.BatchNorm2d(512),
+            # nn.ReLU(),
+            # nn.MaxPool2d(2, 2, 0),       # [512, 4, 4]
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(256*16*16, 4096),
+            nn.BatchNorm1d(4096) if self.linear_batch_norm else nn.Identity(),
+            nn.ReLU(),
+            nn.Dropout(self.linear_dropout_rate) if self.linear_dropout else nn.Identity(),
+
+            nn.Linear(4096, 1024),
+            nn.BatchNorm1d(1024) if self.linear_batch_norm else nn.Identity(),
+            nn.ReLU(),
+            nn.Dropout(self.linear_dropout_rate) if self.linear_dropout else nn.Identity(),
+
+            nn.Linear(1024, 128),
+            nn.ReLU(),
+            nn.Linear(128, 11)
+        )
+
+    def forward(self, x):
+        out = self.cnn(x)
+        out = out.view(out.size()[0], -1)
+        return self.fc(out)
