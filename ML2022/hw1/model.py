56630719e0d6606abf45ff42519c8c7674bcd5b0
oicjacky <610611105@gms.ndhu.edu.tw>
2023-02-16 20:35:08 +0800

    New: My_Model2

--- ML2022/hw1/model.py
+++ ML2022/hw1/model.py
@@ -16,3 +16,26 @@ class My_Model(nn.Module):
         x = self.layers(x)
         x = x.squeeze(1) # (B, 1) -> (B)
         return x
+    
+class My_Model2(nn.Module):
+    def __init__(self, input_dim):
+        super(My_Model2, self).__init__()
+        # TODO: modify model's structure, be aware of dimensions. 
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 4),
+            nn.ReLU(),
+            nn.Linear(4, 1)
+        )
+
+    def forward(self, x):
+        x = self.layers(x)
+        x = x.squeeze(1) # (B, 1) -> (B)
+        return x
